5146d954461a5da4a418bd17084efea777df2217
DByrne <byrne_daniel@hotmail.com>
2023-12-06 10:37:35 +0000

    part 1 solved

--- 6th/main.c
+++ 6th/main.c
@@ -18,12 +18,21 @@ int race(int totalTime, int recordTime)
         }
         
     }
-    return waysToWin
+    return waysToWin;
 }
 
 int main(void)
 {
-    int raceTimes[] = {7,15,30};
-    int recordTimes[] 
+    int raceTimes[] = {42,68,69,85};
+    int recordTimes[] = {284, 1005, 1122, 1341};
+    int total = 1;
+    for (size_t i = 0; i < 4; i++)
+    {
+        int result = race(raceTimes[i], recordTimes[i]);
+        total *= result;
+        printf("Race 1: %d \r\n", result);
+    }
+    printf("Got: %d\r\n", total);
+    
     return 0;
 }
